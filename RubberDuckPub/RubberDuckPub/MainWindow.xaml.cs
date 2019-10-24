73ade7ca79c7244bfcf77637adafc2d347b19c20
Tijana <tijana.ilic87@protonmail.com>
2019-10-24 12:11:58 +0200

    Add basic design and testCases List as items source for combo box

--- RubberDuckPub/RubberDuckPub/MainWindow.xaml.cs
+++ RubberDuckPub/RubberDuckPub/MainWindow.xaml.cs
@@ -23,6 +23,20 @@ namespace RubberDuckPub
         public MainWindow()
         {
             InitializeComponent();
+
+            List<string> testCases = new List<string>()
+            {
+                "Standard Settings",
+                "20 glasses, 3 chairs",
+                "20 chairs, 5 glasses",
+                "Guests staying double time",
+                "The waiter picks upp glasses in half the time",
+                "Bar working hours: 5 minutes",
+                "Couples Night",
+                "Bus comming"
+            };
+            testComboBox.ItemsSource = testCases;
         }
+
     }
 }
