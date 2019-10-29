4cb740e7005890e39ea63890e4ab65d2612479b3
Tijana Ilic <tijana.ilic87@protonmail.com>
2019-10-29 20:21:08 +0100

    Declared mainWindow and bar as fields in Guest class instead of using them as arguments in all the methods

--- RubberDuckPub/RubberDuckPub/Guest.cs
+++ RubberDuckPub/RubberDuckPub/Guest.cs
@@ -7,13 +7,17 @@ namespace RubberDuckPub
     public class Guest
     {
         public string Name { get; set; }
+        public MainWindow mainWindow { get; set; }
+        public Bar bar { get; set; }
         public int TimeToGoToTable { get; set; } = 4000;
         public bool IsInBar { get; set; } = true;
 
         public Guest(string name, Bar bar, MainWindow mainWindow)
         {
             Name = name;
-            StartGuest(bar, mainWindow);
+            this.mainWindow = mainWindow;
+            this.bar = bar;
+            StartGuest();
             //Task.Run(() =>
             //{
             //    while (IsInBar)
@@ -32,7 +36,7 @@ namespace RubberDuckPub
             //});
         }
 
-        private void StartGuest(Bar bar, MainWindow mainWindow)
+        private void StartGuest()
         {
             Task.Run(() =>
             {
@@ -44,50 +48,48 @@ namespace RubberDuckPub
                         bar.guestWaitingForTableQueue.TryDequeue(out nextToBeServed);
                         if (nextToBeServed != null)
                         {
-                            Log(DateTime.Now, $"{nextToBeServed.Name} is searching for an available seat.", mainWindow);
-                            SearchForEmptyChair(bar, mainWindow, nextToBeServed);
+                            Log(DateTime.Now, $"{nextToBeServed.Name} is searching for an available seat.");
+                            SearchForEmptyChair(nextToBeServed);
                         }
                     }
                 }
             });
         }
 
-        private void SearchForEmptyChair(Bar bar, MainWindow mainWindow, Guest nextToBeSeated)
+        private void SearchForEmptyChair(Guest nextToBeSeated)
         {
             if (bar.emptyChairs.Count > 0)
             {
                 Thread.Sleep(TimeToGoToTable);
-                Log(DateTime.Now, $"{nextToBeSeated.Name} is sitting at the table.", mainWindow);
+                Log(DateTime.Now, $"{nextToBeSeated.Name} is sitting at the table.");
                 bar.seatedGuests.Add(nextToBeSeated);
                 bar.emptyChairs.TryPop(out Chairs removedChair);
-                ////mainWindow.Dispatcher.Invoke(() => bar.BarContentInfo(mainWindow, bar.cleanGlassesStack.Count, bar.emptyChairs.Count));
-                DrinkBeer(bar, mainWindow, removedChair, nextToBeSeated);
+                DrinkBeer(removedChair, nextToBeSeated);
             }
         }
 
-        private void DrinkBeer(Bar bar, MainWindow mainWindow, Chairs removedChair, Guest drinkingGuest)
+        private void DrinkBeer(Chairs removedChair, Guest drinkingGuest)
         {
             Random r = new Random();
-            Log(DateTime.Now, $"{drinkingGuest.Name} is drinking beer", mainWindow);
+            Log(DateTime.Now, $"{drinkingGuest.Name} is drinking beer");
             int secondsToDrinkBeer = r.Next(20, 31);
             if (bar.GuestsStayingDouble)
                 secondsToDrinkBeer *= 2;
             Thread.Sleep(secondsToDrinkBeer * 1000);
-            GoHome(bar, mainWindow, removedChair, drinkingGuest);
+            GoHome(removedChair, drinkingGuest);
         }
 
-        private void GoHome(Bar bar, MainWindow mainWindow, Chairs removedChair, Guest leavingGuest)
+        private void GoHome(Chairs removedChair, Guest leavingGuest)
         {
             bar.seatedGuests.Remove(this);
-            Log(DateTime.Now, $"{leavingGuest.Name} finished the beer and goes home.", mainWindow);
+            Log(DateTime.Now, $"{leavingGuest.Name} finished the beer and goes home.");
             IsInBar = false;
             bar.dirtyGlassesStack.Push(new Glasses());
             bar.emptyChairs.Push(removedChair);
             bar.TotalNumberGuests--;
-            ////mainWindow.Dispatcher.Invoke(() => bar.BarContentInfo(mainWindow, bar.cleanGlassesStack.Count, bar.emptyChairs.Count));
         }
 
-        private void Log(DateTime timestamp, string activity, MainWindow mainWindow)
+        private void Log(DateTime timestamp, string activity)
         {
             mainWindow.Dispatcher.Invoke(() => mainWindow.GuestsListBox.Items.Insert(0, $"{timestamp.ToString("H:mm:ss")} - {activity}"));
         }
